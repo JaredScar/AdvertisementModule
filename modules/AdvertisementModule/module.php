6406713392882d761af35d263125d72567fed39d
Jared <TheWolfBadger@gmail.com>
2022-04-08 12:49:02 -0400

    Current changes so far

--- modules/AdvertisementModule/module.php
+++ modules/AdvertisementModule/module.php
@@ -14,10 +14,10 @@ class AdvertisementModule extends Module {
 
         parent::__construct($this, $name, $author, $module_version, $nameless_version);
 
-        $this->pages->add($name, '/panel/ad_settings', 'pages/panel/advertisement_settings.php');
-        $this->pages->add($name, '/panel/ad_add', 'pages/panel/advertisement_add.php');
-        $this->pages->add($name, '/panel/ad_delete', 'pages/panel/advertisement_confirm_delete.php');
-        $this->pages->add($name, '/ad_stats', 'pages/advertisement_stats.php');
+        $this->pages->add($name, '/panel/ad_settings', 'pages/panel/advertisement_settings.php', 'Advertisement Settings', false);
+        $this->pages->add($name, '/panel/ad_add', 'pages/panel/advertisement_add.php', 'Add Advertisement', false);
+        $this->pages->add($name, '/panel/ad_delete', 'pages/panel/advertisement_delete.php', 'Delete Advertisement', false);
+        $this->pages->add($name, '/ad_stats', 'pages/advertisement_stats.php', 'Advertisement Stats', false);
     }
 
     public function onInstall() {}
@@ -62,11 +62,51 @@ class AdvertisementModule extends Module {
     public function onDisable() {}
 
     public function onPageLoad($user, $pages, $cache, $smarty, $navs, $widgets, $template) {
+        /**
+         * $user
+         * ------------------------------------------------------
+         * data() => brings back all fields within the nl2_users database column
+         * hasPermission($permission) => returns true of false if the $user has the $permission
+         *
+         * $pages
+         * ------------------------------------------------------
+         * add($module, $url, $file, $name = '', $widgets = false)
+         *
+         * $cache
+         * ------------------------------------------------------
+         * setCache($name)
+         * isCached($key)
+         * retrieve($key)
+         * store($key, $value, $expiration = 0) => If expiration is set to 0, it will never expire
+         *
+         * $smarty
+         * ------------------------------------------------------
+         * N/A
+         *
+         * $navs
+         * ------------------------------------------------------
+         * $navs[0] top navigation bar on frontend.
+         * $navs[1] user dropdown on frontend.
+         * $navs[2] StaffCP nav sidebar.
+         * add($name, $title, $link, $location = 'top', $target = null, $order = 10, $icon = '');
+         *
+         * $widgets
+         * ------------------------------------------------------
+         * add($widget) => $widget must be an instance of a class which extends the WidgetBase class
+         * getPages($name) => Returns an array of page names that the widget named $name is enabled on
+         *
+         * $template
+         * ------------------------------------------------------
+         * N/A
+         */
         PermissionHandler::registerPermissions($this->getName(), [
             self::$CREATE_PERMISSION => 'Ability to create new advertisements',
             self::$DELETE_PERMISSION => 'Ability to delete advertisements and their data',
             self::$VIEW_PERMISSION => "Ability to view advertisements' statistics"
         ]);
+        $navs[2]->add('create_ad', 'Create Ad', URL::build('/panel/ad_add'));
+        $navs[2]->add('settings_ad', 'Ad Settings', URL::build('/panel/ad_settings'));
+        $navs[2]->add('delete_ad', 'Delete Ad', URL::build('/panel/ad_delete'));
     }
 
 }
